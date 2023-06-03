32f8f1e8bd58a42aa2590c589077c0dd86de9858
Pâmela Leles <pamela_leles_sousa@outlook.com>
2023-06-03 19:42:57 -0300

    First commit

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <View>
+        <Text>Master Control</Text>
+        <Text>Remote DeskService Suite</Text>
+      </View>
+      <View>
+        <Text> Email (Seu e-mail será seu login no sistema)</Text>
+        <Text> ____________________________________________</Text>
+      </View>
+      <View>
+        <Text> Senha (Mínimo de 10 caracteres ou digitos)</Text>
+        <Text> ___________________________________________</Text>
+      </View>
+      <View>
+        <Text> Nome ou Razão social</Text>
+        <Text> ____________________ </Text>
+      </View>
+      <View>
+        <Text> Telefone para contato</Text>
+        <Text> _____________________ </Text>
+      </View>
+    </View>
+  );
+}
+
+export default App;
